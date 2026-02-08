758c726c52c786230de3c97397a36057b8e2b823
nawshadpro01-gif <nawshadprofessional01@gmail.com>
2026-02-08 15:18:40 +0600

    2nd commit

--- final.js
+++ final.js
@@ -1,7 +1,6 @@
 /*function signature/sample */
 // Problem 1
 function newPrice(currentPrice , discount ) {
-
     if(typeof currentPrice  !=="number"||typeof discount  !=="number"){
         return"Invalid";
     }
@@ -10,9 +9,22 @@ function newPrice(currentPrice , discount ) {
     }
 
 
+
     let discountAmount =(currentPrice * discount)/ 100;
     let finalPrice =currentPrice- discountAmount;
     return finalPrice.toFixed(3);
 
           // You have to write your code here
+}
+
+
+// problem 2
+function validOtp(otp){
+    if (typeof otp !== "string"){
+        return "Invalid"
+    }
+    else if(otp.length===8 && otp.startsWith("ph-")){
+        return true
+    }
+    else{return false;}
 }
